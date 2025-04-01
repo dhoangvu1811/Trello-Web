e8285eced04355c20a531880cd727f50aed5c0b2
dhoangvudev <vudh.22itb@vku.udn.vn>
2025-04-01 21:42:35 +0700

    Refactor Project Structure

---
+++ src/components/ModeSelect/index.jsx
@@ -0,0 +1,50 @@
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  useColorScheme
+} from '@mui/material'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+function ModeSelect() {
+  const { mode, setMode } = useColorScheme()
+
+  const handleChange = (event) => {
+    const selectMode = event.target.value
+    setMode(selectMode)
+  }
+
+  return (
+    <FormControl sx={{ m: 1, minWidth: 120 }} size='small'>
+      <InputLabel id='label-select-dark-light-mode'>Mode</InputLabel>
+      <Select
+        labelId='label-select-dark-light-mode'
+        id='select-dark-light-mode'
+        value={mode}
+        label='Mode'
+        onChange={handleChange}
+      >
+        <MenuItem value='light'>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <LightModeIcon fontSize='small' /> Light
+          </div>
+        </MenuItem>
+        <MenuItem value='dark'>
+          <Box sx={{ display: 'flex', alignItems: 'center', gap: 1 }}>
+            <DarkModeIcon fontSize='small' /> Dark
+          </Box>
+        </MenuItem>
+        <MenuItem value='system'>
+          <div style={{ display: 'flex', alignItems: 'center', gap: '8px' }}>
+            <SettingsBrightnessIcon fontSize='small' /> System
+          </div>
+        </MenuItem>
+      </Select>
+    </FormControl>
+  )
+}
+
+export default ModeSelect
