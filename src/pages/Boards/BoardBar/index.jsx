a17107e9ee7e224f070b13f560885f7f022201ea
dhoangvudev <vudh.22itb@vku.udn.vn>
2025-04-06 16:27:48 +0700

    Board Bar

--- src/pages/Boards/BoardBar/index.jsx
+++ src/pages/Boards/BoardBar/index.jsx
@@ -1,17 +1,122 @@
-import { Box } from '@mui/material'
+import { Avatar, AvatarGroup, Box, Button, Chip, Tooltip } from '@mui/material'
+import DashboardIcon from '@mui/icons-material/Dashboard'
+import VpnLockIcon from '@mui/icons-material/VpnLock'
+import AddToDriveIcon from '@mui/icons-material/AddToDrive'
+import BoltIcon from '@mui/icons-material/Bolt'
+import FilterListIcon from '@mui/icons-material/FilterList'
+import PersonAddIcon from '@mui/icons-material/PersonAdd'
+
+const MENU_STYLES = {
+  color: 'primary.main',
+  bgcolor: 'white',
+  paddingX: '5px',
+  border: 'none',
+  borderRadius: '4px',
+  '& .MuiSvgIcon-root': {
+    color: 'primary.main'
+  },
+  '&:hover': {
+    bgcolor: 'primary.50'
+  }
+}
 
 function BoardBar() {
   return (
     <Box
       sx={{
-        backgroundColor: 'primary.dark',
+        paddingX: 2,
         width: '100%',
         height: (theme) => theme.trello.boardBarHeight,
         display: 'flex',
-        alignItems: 'center'
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        gap: 2,
+        overflowX: 'auto',
+        borderTop: '1px solid #00bfa5'
       }}
     >
-      Board Bar
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: 2
+        }}
+      >
+        <Chip
+          sx={MENU_STYLES}
+          icon={<DashboardIcon />}
+          label='DHV MERN Stack Board'
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<VpnLockIcon />}
+          label='Public/Private WorkSpace'
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<AddToDriveIcon />}
+          label='Add To Google Drive'
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<BoltIcon />}
+          label='Automation'
+          clickable
+        />
+        <Chip
+          sx={MENU_STYLES}
+          icon={<FilterListIcon />}
+          label='Filter'
+          clickable
+        />
+      </Box>
+
+      <Box
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          gap: 2
+        }}
+      >
+        <Button variant='outlined' startIcon={<PersonAddIcon />}>
+          Invite
+        </Button>
+
+        <AvatarGroup
+          max={7}
+          sx={{
+            '& .MuiAvatar-root ': { width: 34, height: 34, fontSize1: 16 }
+          }}
+        >
+          <Tooltip title='DHVDev'>
+            <Avatar alt='DHVDev' src='/static/images/avatar/1.jpg' />
+          </Tooltip>
+          <Tooltip title='DHVDev'>
+            <Avatar alt='DHVDev' src='/static/images/avatar/1.jpg' />
+          </Tooltip>
+          <Tooltip title='DHVDev'>
+            <Avatar alt='DHVDev' src='/static/images/avatar/1.jpg' />
+          </Tooltip>
+          <Tooltip title='DHVDev'>
+            <Avatar alt='DHVDev' src='/static/images/avatar/1.jpg' />
+          </Tooltip>
+          <Tooltip title='DHVDev'>
+            <Avatar alt='DHVDev' src='/static/images/avatar/1.jpg' />
+          </Tooltip>
+          <Tooltip title='DHVDev'>
+            <Avatar alt='DHVDev' src='/static/images/avatar/1.jpg' />
+          </Tooltip>
+          <Tooltip title='DHVDev'>
+            <Avatar alt='DHVDev' src='/static/images/avatar/1.jpg' />
+          </Tooltip>
+          <Tooltip title='DHVDev'>
+            <Avatar alt='DHVDev' src='/static/images/avatar/1.jpg' />
+          </Tooltip>
+        </AvatarGroup>
+      </Box>
     </Box>
   )
 }
