70e5d958d0120638548336abf6b6d6f4779da485
dhoangvudev <vudh.22itb@vku.udn.vn>
2025-04-30 15:03:40 +0700

    Add axios dependency and implement fetchBoardDetailsAPI function

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -2,14 +2,26 @@ import { Container } from '@mui/material'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardBar from './BoardBar/BoardBar'
 import BoardContent from './BoardContent/BoardContent'
-import { mockData } from '~/apis/mock-data'
+// import { mockData } from '~/apis/mock-data'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    const baordId = '6811cd4fc786c39bf7d2d6ac'
+    // Call Api
+    fetchBoardDetailsAPI(baordId).then((borad) => {
+      setBoard(board)
+    })
+  }, [])
+
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoardBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      <BoardBar board={board} />
+      <BoardContent board={board} />
     </Container>
   )
 }
