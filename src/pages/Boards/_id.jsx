2f63c25eeb56844011525eb35293cb16814fcd94
dhoangvudev <vudh.22itb@vku.udn.vn>
2025-05-01 09:03:23 +0700

    Add tooltip to Chip component in BoardBar and update board ID in Board component

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -10,7 +10,7 @@ function Board() {
   const [board, setBoard] = useState(null)
 
   useEffect(() => {
-    const baordId = '6811e4d2cfbbe789380a8130'
+    const baordId = '6812c78b8fbd39624638ddbf'
     // Call Api
     fetchBoardDetailsAPI(baordId).then((board) => {
       setBoard(board)
