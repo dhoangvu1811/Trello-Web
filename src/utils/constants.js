2b5d216b662773313c103174896ec278e581e5bb
dhoangvudev <vudh.22itb@vku.udn.vn>
2025-05-06 23:45:32 +0700

    deploy_prod

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,10 @@
-export const API_ROOT = 'http://localhost:8017'
+/* có thể dùng import.meta.env của vite */
+
+let apiRoot = ''
+if (process.env.BUILD_MODE === 'dev') {
+  apiRoot = 'http://localhost:8017'
+}
+if (process.env.BUILD_MODE === 'production') {
+  apiRoot = 'https://trello-api-5q6o.onrender.com'
+}
+export const API_ROOT = apiRoot
