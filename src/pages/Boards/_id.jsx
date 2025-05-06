9701da6d136e93548c7f4c39a1b80f406977007d
dhoangvudev <vudh.22itb@vku.udn.vn>
2025-05-06 09:39:04 +0700

    Fix typo in variable name for boardId in fetchBoardDetailsAPI call

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -20,9 +20,9 @@ function Board() {
   const [board, setBoard] = useState(null)
 
   useEffect(() => {
-    const baordId = '6812c78b8fbd39624638ddbf'
+    const boardId = '6812c78b8fbd39624638ddbf'
     // Call Api
-    fetchBoardDetailsAPI(baordId).then((board) => {
+    fetchBoardDetailsAPI(boardId).then((board) => {
       //Sắp xếp các column trước khi đưa xuống các component con
       board.columns = mapOrder(board.columns, board?.columnOrderIds, '_id')
 
