6d24f3a3253bd3206d7159dc7c3e8b26fe61d571
dhoangvudev <vudh.22itb@vku.udn.vn>
2025-05-29 17:04:02 +0700

    feat: Integrate redux-persist for state management and implement protected routes for authentication

--- src/main.jsx
+++ src/main.jsx
@@ -9,27 +9,33 @@ import { ConfirmProvider } from 'material-ui-confirm'
 import { store } from '~/redux/store'
 import { Provider } from 'react-redux'
 import { BrowserRouter } from 'react-router-dom'
+//Cấu hình Redux-Persist
+import { PersistGate } from 'redux-persist/integration/react'
+import { persistStore } from 'redux-persist'
+const persistor = persistStore(store)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <BrowserRouter basename='/'>
     <Provider store={store}>
-      <CssVarsProvider theme={theme}>
-        <ConfirmProvider
-          defaultOptions={{
-            allowClose: false,
-            dialogProps: { maxWidth: 'xs' },
-            confirmationButtonProps: {
-              color: 'secondary',
-              variant: 'outlined'
-            },
-            cancellationButtonProps: { color: 'inherit' }
-          }}
-        >
-          <CssBaseline />
-          <App />
-          <ToastContainer theme='colored' position='bottom-left' />
-        </ConfirmProvider>
-      </CssVarsProvider>
+      <PersistGate persistor={persistor}>
+        <CssVarsProvider theme={theme}>
+          <ConfirmProvider
+            defaultOptions={{
+              allowClose: false,
+              dialogProps: { maxWidth: 'xs' },
+              confirmationButtonProps: {
+                color: 'secondary',
+                variant: 'outlined'
+              },
+              cancellationButtonProps: { color: 'inherit' }
+            }}
+          >
+            <CssBaseline />
+            <App />
+            <ToastContainer theme='colored' position='bottom-left' />
+          </ConfirmProvider>
+        </CssVarsProvider>
+      </PersistGate>
     </Provider>
   </BrowserRouter>
 )
--- src/redux/store.js
+++ src/redux/store.js
@@ -2,9 +2,30 @@ import { configureStore } from '@reduxjs/toolkit'
 import { activeBoardReducer } from './activeBoard/activeBoardSlice'
 import { userReducer } from './user/userSlice'
 
+//Cấu hình redux-persist
+import { combineReducers } from 'redux'
+import { persistReducer } from 'redux-persist'
+import storage from 'redux-persist/lib/storage' //default là localstorage
+
+// Cấu hình Persist
+const rootPersistConfig = {
+  key: 'root', // key của persist do chúng ta chỉ định cứ để mặc định là root
+  storage: storage, // Lưu vào localstorage
+  whiteList: ['user'] // định nghĩa các slice ĐƯỢC PHÉP duy trì qua mỗi lần f5 trình duyệt
+  // blackList: ['user'] // định nghĩa các slice KHÔNG ĐƯỢC PHÉP duy trì qua mỗi lần f5 trình duyệt
+}
+
+//Combine các reducers trong dự án của chúng ta
+const reducers = combineReducers({
+  activeBoard: activeBoardReducer,
+  user: userReducer
+})
+
+//Thực hiện persist Reducer
+const persistReducers = persistReducer(rootPersistConfig, reducers)
+
 export const store = configureStore({
-  reducer: {
-    activeBoard: activeBoardReducer,
-    user: userReducer
-  }
+  reducer: persistReducers,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({ serializableCheck: false })
 })
